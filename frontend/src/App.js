490a81998c24f3bfe60678a370d439655ef7f86a
emilyychenn <emilylynnchen@gmail.com>
2022-03-24 19:30:04 -0700

    boostrapped frontend and deployed through gh-pages

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          CPSC 304 Project - jSearch!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
