9bd6e883113bca6a20a3a8ccf83ba985b2a50e58
emilyychenn <emilylynnchen@gmail.com>
2022-03-31 17:46:54 -0700

    added enum for status

--- src/main/java/jSearch/models/ApplicationMade.java
+++ src/main/java/jSearch/models/ApplicationMade.java
@@ -5,23 +5,23 @@ import java.util.UUID;
 
 public class ApplicationMade {
     UUID application_id;
-    Enum status;
+    Status status_description;
     int resume_version;
     int cover_letter_version;
     Date date_of_application;
     UUID applicant_id;
     UUID position_id;
 
-    public ApplicationMade(UUID application_id, Enum status, int resume_version, int cover_letter_version) {
+    public ApplicationMade(UUID application_id, Status status, int resume_version, int cover_letter_version) {
         this.application_id = application_id;
-        this.status = status;
+        this.status_description = status;
         this.resume_version = resume_version;
         this.cover_letter_version = cover_letter_version;
     }
 
-    public ApplicationMade(UUID application_id, Enum status, int resume_version, int cover_letter_version, Date date_of_application, UUID applicant_id, UUID position_id) {
+    public ApplicationMade(UUID application_id, Status status, int resume_version, int cover_letter_version, Date date_of_application, UUID applicant_id, UUID position_id) {
         this.application_id = application_id;
-        this.status = status;
+        this.status_description = status;
         this.resume_version = resume_version;
         this.cover_letter_version = cover_letter_version;
         this.date_of_application = date_of_application;
