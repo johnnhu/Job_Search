514cd9de42205da3505f0503f770d658cabd2280
emilyychenn <emilylynnchen@gmail.com>
2022-04-02 23:51:53 -0700

    removed extra comments

--- src/main/java/jSearch/models/SpecializationInfo.java
+++ src/main/java/jSearch/models/SpecializationInfo.java
@@ -9,9 +9,6 @@ public class SpecializationInfo {
     public boolean is_honours;
     public String degree_type;
 
-    // constructor for all fields that cannot be NULL
-    // TODO: add checks in models for not null
-    // TODO: add methods in models to get functional dependencies (e.g. given major, is_honours, degree_type, get numCredits?)
     public SpecializationInfo(UUID spec_id, String major, String minor, boolean is_honours, String degree_type) {
         this.spec_id = spec_id;
         this.major = major;
